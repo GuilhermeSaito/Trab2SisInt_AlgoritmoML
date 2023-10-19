c179fb25cf9d078c33337b3752e79198473e87d6
gtsaito <gtsaito@mppr.mp.br>
2023-10-19 14:19:52 -0300

    Arrumado o codigo de redes neurais

--- rede_neural.py
+++ rede_neural.py
@@ -1,20 +1,41 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.metrics import mean_squared_error, accuracy_score
 
-data = pd.read_csv("treino_sinais_vitais_com_label.txt")
-data = data.drop(data.columns[[0, 1, 2]], axis = 1)
+def train_continuos_values(x, y):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# print(data.to_string(index = False))
+    model = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=1000, random_state=1)
+    model.fit(X_train, y_train)
 
-x = data.iloc[:, [0, 1, 2, 3]]
-y = data.iloc[:, 4]
+    y_pred = model.predict(X_test)
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f'Erro Médio Quadrático (MSE): {mse:.2f}')
 
-model = MLPClassifier(hidden_layer_sizes=(50, 50), max_iter=1000, random_state=1)
+def train_discret_values(x, y):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model.fit(X_train, y_train)
+    model = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=1000, random_state=1)
+    model.fit(X_train, y_train)
 
-accuracy = model.score(X_test, y_test)
-print(f'Acurácia do modelo: {accuracy:.2f}')
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'Acurácia do modelo: {accuracy:.2f}')
+
+
+nomes_colhnas_com_label = ["qPA", "pulso", "freq", "gravidade", "classes"]
+data = pd.read_csv("treino_sinais_vitais_com_label.txt", header = None, names = nomes_colhnas_com_label)
+
+x = data.loc[:, "qPA" : "freq"]
+y = data.loc[:, "gravidade"]
+y_classe = data.loc[:, "classes"]
+
+x = x.reset_index(drop=True)
+y = y.reset_index(drop=True)
+y_classe = y_classe.reset_index(drop=True)
+
+train_continuos_values(x, y)
+train_discret_values(x, y_classe.values)
